3d353f73f30137186037b1060f945dbe3361bc5a
ben farrell <ben.farrell08@gmail.com>
2021-02-19 14:25:51 +0000

    All recipes listed on homepage. Each recipe has hyperlink to ingredients

--- recipe/urls.py
+++ recipe/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from .views import RecipeDetailView
 
 urlpatterns = [
     path('', views.listRecipe, name='recipes'),
+    path('recipe/<int:pk>/',views.RecipeView, name='detail_recipe'),
+    # path('recipe/<int:pk>/',RecipeDetailView.as_view(),name='detail_recipe'),
 ]
--- recipe/views.py
+++ recipe/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
+from .models import Recipe, Ingredient
+from django.views.generic import DetailView, CreateView, UpdateView
 
 # Create your views here.
 
 def listRecipe(request):
 
-	recipes = ['Sausage & Mash','Pizza','Pasta']
+	recipes = Recipe.objects.all()
 
 	return render(request,'recipe/home.html',context={'recipes':recipes})
+
+class RecipeDetailView(DetailView):
+	model = Recipe
+	template_name = 'recipe/recipe_DetailView.html' #This is standard naming convention for class based view
+
+def RecipeView(request, pk):
+
+	recipe = ' '.join(Recipe.objects.filter(id=pk)[0].title.split('_'))
+	ingredients = Ingredient.objects.filter(recipe_id=pk)
+
+	context = {'recipe':recipe, 'ingredients':ingredients}
+
+	return render(request, 'recipe/recipe_DetailView.html', context=context)
