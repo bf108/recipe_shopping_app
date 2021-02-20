d9c266e52cde5686f2b6dd13c673b4c11e96e078
ben farrell <ben.farrell08@gmail.com>
2021-02-20 09:27:35 +0000

    Added crispy-forms to app

--- shop_App/settings.py
+++ shop_App/settings.py
@@ -31,13 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'crispy_forms',
+    'recipe.apps.RecipeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'recipe.apps.RecipeConfig',
+    
 ]
 
 MIDDLEWARE = [
