df74f4cda8008ef90454659ca139382a88b780b8
ben farrell <ben.farrell08@gmail.com>
2021-02-23 19:05:42 +0000

    Cleared up urls to several views

--- recipe/urls.py
+++ recipe/urls.py
@@ -3,19 +3,19 @@ from . import views
 from .views import (RecipeDetailView, 
 	RecipeListView,
     RecipeCreateView,
+    RecipeUpdateView,
 	IngredientCreateView,
 	IngredientUpdateView,
 	IngredientDeleteView)
 
 urlpatterns = [
-    # path('', views.listRecipe, name='recipes'),
     path('',RecipeListView.as_view(), name='recipe_list'),
-    # path('recipe/<int:pk>/',views.RecipeView, name='detail_recipe'),
-    path('recipe/<str:title>/',views.RecipeView, name='detail_recipe'),
+    path('recipe/update/<str:title>/<int:pk>/',RecipeUpdateView.as_view(),name='recipe_update'),
+    path('create_recipe/',RecipeCreateView.as_view(), name='recipe_create'),
+    path('recipe/<str:title>/<int:pk>/',RecipeDetailView.as_view(),name='recipe_detail'),
     path('recipe/<str:title>/add_ingredient/', IngredientCreateView.as_view(), name='ingredient_create'),
     path('recipe/<str:title>/update/<int:pk>/', IngredientUpdateView.as_view(), name='ingredient_update'),
     path('recipe/<str:title>/delete/<int:pk>/', IngredientDeleteView.as_view(), name='ingredient_delete'),
-    path('create_recipe/',RecipeCreateView.as_view(), name='recipe_create'),
-    # path('recipe/<int:pk>/',RecipeDetailView.as_view(),name='detail_recipe'),
+
 
 ]
