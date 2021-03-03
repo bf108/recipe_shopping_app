da49dd8bdadedea4fe1ef7393a0396a83518ecb6
ben farrell <ben.farrell08@gmail.com>
2021-03-03 16:45:08 +0000

    Update admin.py to allow multichoice meal selection in shopping list

--- recipe/admin.py
+++ recipe/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from .models import Recipe, Ingredient, ShoppingList
+from django.db import models
+from django.forms.widgets import CheckboxSelectMultiple
 
 # Register your models here.
 admin.site.register(Recipe)
 admin.site.register(Ingredient)
-admin.site.register(ShoppingList)
+
+class ShoppingListAdmin(admin.ModelAdmin):
+	formfield_overrides = {models.ManyToManyField: {'widget': CheckboxSelectMultiple}}
+
+admin.site.register(ShoppingList, ShoppingListAdmin)
